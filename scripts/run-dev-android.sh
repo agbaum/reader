cd766431e6bb9dd21d4f43b863e5c683c61853cc
Adam Gruenbaum <akpgreentree@gmail.com>
2026-03-31 17:49:04 -0700

    Make run-dev-android uninstall existing app first to avoid signature conflict

--- scripts/run-dev-android.sh
+++ scripts/run-dev-android.sh
@@ -6,6 +6,9 @@ echo "Switching to dev package id..."
 pnpm run toggle-package-id dev
 
 echo "Running app on Android..."
+# Ensure old signed app does not block install
+adb uninstall com.akpgreentree.reader || true
+
 pnpm expo run:android
 
 echo "Switching back to prod package id..."
